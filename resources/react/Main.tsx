866264eb7c700b61fd031d526dcc407a518eaf5b
youssef bibawy <youssefbibawy@gmail.com>
2024-03-30 08:13:42 +0200

    add more pages and fix more type errors

--- resources/react/Main.tsx
+++ resources/react/Main.tsx
@@ -2,6 +2,9 @@ import { createRoot } from 'react-dom/client'
 import { createInertiaApp } from '@inertiajs/inertia-react'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 
+// Assets
+import "@/styles/style.scss";
+
 createInertiaApp({
   // @ts-ignore
   resolve: (name) => resolvePageComponent(`./Pages/${name}/index.tsx`, import.meta.glob('./Pages/**/*.tsx')),
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+import path from "path";
+
+export default defineConfig({
+  plugins: [
+    laravel({
+      input: ['resources/react/Main.tsx'],
+      refresh: true,
+    }),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      // @ts-ignore
+      '@/': `${path.resolve(__dirname, './resources/react')}/`,
+      // @ts-ignore
+      '~/': `${path.resolve(__dirname, './public')}/`,
+    }
+  },
+});
